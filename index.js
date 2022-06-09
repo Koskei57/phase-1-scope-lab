2ed69afa17de71f7a5401c9920e4b8efad183dfe
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-09 05:25:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+/*function myFunc() {
+return 'bob';
+}*/
+var customerName = "bob";
+console.log(customerName);
+
+function upperCaseCustomerName() {
+    customerName = (customerName.toUpperCase());
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+const leastFavoriteCustomer = 'bob';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'deno'
+    return leastFavoriteCustomer;
+}
